1f026bbfe7fa1d0b7f9416d10f6b7033e17c9dd3
Dave Harris <david.harris@youdo.co.nz>
2017-07-14 21:41:26 +1200

    Graph solids per week

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -7,31 +7,39 @@ class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sum_amount_per_week: []
+      bottles_per_week: [],
+      solids_per_week: []
     };
   }
 
   componentDidMount() {
-    APIActions.fetchJSON("/bottles").then(data => {
+    APIActions.fetchJSON("/bottles/week").then(data => {
       this.setState({
-        sum_amount_per_week: data.sum_amount_per_week
+        bottles_per_week: data
+      });
+    });
+
+    APIActions.fetchJSON("/solids/week").then(data => {
+      this.setState({
+        solids_per_week: data
       });
     });
   }
 
   renderGraph = () => {
-    const dates = Object.keys(this.state.sum_amount_per_week).map(date => {
+    const dates = Object.keys(this.state.bottles_per_week).map(date => {
       return moment(date, "YYYY-MM-DD").format("Do MMMM YYYY");
     });
 
-    const weekly_frequencies = Object.values(this.state.sum_amount_per_week);
+    const bottles_data = Object.values(this.state.bottles_per_week);
+    const solids_data = Object.values(this.state.solids_per_week);
 
     const data = {
       labels: dates,
       datasets: [
         {
-          label: "Total ml per Week",
-          data: weekly_frequencies,
+          label: "Total bottles (ml) per Week",
+          data: bottles_data,
           fill: false,
           lineTension: 0.1,
           backgroundColor: "rgba(75,192,192,0.4)",
@@ -46,6 +54,27 @@ class Graph extends React.Component {
           pointHoverRadius: 5,
           pointHoverBackgroundColor: "rgba(75,192,192,1)",
           pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10
+        },
+        {
+          label: "Total solids (gm) per Week",
+          data: solids_data,
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(255,99,132,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(255,99,132,0.4)",
+          pointHoverBorderColor: "rgba(255,99,132,1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10
